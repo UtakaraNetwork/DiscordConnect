a8e24d75fce69c629a78f8df65cdd967ee15c3f4
Necnion8 <necnionch@gmail.com>
2023-05-16 17:24:49 +0900

    [add]管理者チャットDiscordチャンネルの発言をゲーム内に送る

--- src/main/java/work/novablog/mcplugin/discordconnect/listener/DiscordListener.java
+++ src/main/java/work/novablog/mcplugin/discordconnect/listener/DiscordListener.java
@@ -29,6 +29,8 @@ public class DiscordListener extends ListenerAdapter {
     private final DiscordCommandExecutor discordCommandExecutor;
     private final Long consoleChannelId;
     private final Boolean allowDispatchCommandFromConsoleChannel;
+    private final String toMinecraftFormatAdminChat;
+    private final long adminChatChannelId;
 
     /**
      * Discordのイベントをリッスンするインスタンスを生成します
@@ -54,6 +56,40 @@ public class DiscordListener extends ListenerAdapter {
         this.discordCommandExecutor = discordCommandExecutor;
         this.consoleChannelId = consoleChannelId;
         this.allowDispatchCommandFromConsoleChannel = allowDispatchCommandFromConsoleChannel;
+        this.toMinecraftFormatAdminChat = null;
+        this.adminChatChannelId = -1;
+    }
+
+    /**
+     * Discordのイベントをリッスンするインスタンスを生成します
+     *
+     * @param prefix                   コマンドのprefix
+     * @param toMinecraftFormat        DiscordのメッセージをBungeecordへ転送するときのフォーマット
+     * @param fromDiscordToDiscordName Discordのメッセージを再送するときの名前欄のフォーマット
+     * @param discordCommandExecutor   discordのコマンドの解析や実行を行うインスタンス
+     * @param consoleChannelId         コンソールチャンネルのID
+     * @param allowDispatchCommandFromConsoleChannel コンソールチャンネルからのコマンド実行を許可するか否か
+     * @param toMinecraftFormatAdminChat DiscordのメッセージをBungeecordへ転送するときのフォーマット (管理者チャット)
+     * @param adminChatChannelId       管理者チャットの出力チャンネルID
+     */
+    public DiscordListener(
+            @NotNull String prefix,
+            @NotNull String toMinecraftFormat,
+            @NotNull String fromDiscordToDiscordName,
+            @NotNull DiscordCommandExecutor discordCommandExecutor,
+            @Nullable Long consoleChannelId,
+            @Nullable Boolean allowDispatchCommandFromConsoleChannel,
+            @Nullable String toMinecraftFormatAdminChat,
+            long adminChatChannelId
+    ) {
+        this.prefix = prefix;
+        this.toMinecraftFormat = toMinecraftFormat;
+        this.fromDiscordToDiscordName = fromDiscordToDiscordName;
+        this.discordCommandExecutor = discordCommandExecutor;
+        this.consoleChannelId = consoleChannelId;
+        this.allowDispatchCommandFromConsoleChannel = allowDispatchCommandFromConsoleChannel;
+        this.toMinecraftFormatAdminChat = toMinecraftFormatAdminChat;
+        this.adminChatChannelId = adminChatChannelId;
     }
 
     @Override
@@ -61,8 +97,10 @@ public class DiscordListener extends ListenerAdapter {
         if (receivedMessage.getAuthor().isBot()) return;
         BotManager botManager = DiscordConnect.getInstance().getBotManager();
         assert botManager != null;
-        if (botManager.getChatChannelSenders().stream()
-                .noneMatch(sender -> sender.getChannelID() == receivedMessage.getChannel().getIdLong()) && !(consoleChannelId == receivedMessage.getChannel().getIdLong()))
+        if (botManager.getChatChannelSenders().stream().noneMatch(sender -> sender.getChannelID() == receivedMessage.getChannel().getIdLong())
+                && !(consoleChannelId == receivedMessage.getChannel().getIdLong())
+                && adminChatChannelId != receivedMessage.getChannel().getIdLong()
+        )
             return;
 
         if (receivedMessage.getMessage().getContentRaw().startsWith(prefix)) {
@@ -92,11 +130,15 @@ public class DiscordListener extends ListenerAdapter {
                 );
             }
         } else {
+            boolean isAdminChat = adminChatChannelId == receivedMessage.getChannel().getIdLong();
+            if (isAdminChat && toMinecraftFormatAdminChat == null)
+                return;
+
             String name = receivedMessage.getAuthor().getName();
             String nickname = Objects.requireNonNull(receivedMessage.getMember()).getNickname() == null ?
                     name : Objects.requireNonNull(receivedMessage.getMember()).getNickname();
 
-            String formattedForMinecraft = toMinecraftFormat
+            String formattedForMinecraft = (isAdminChat ? toMinecraftFormatAdminChat : toMinecraftFormat)
                     .replace("{name}", receivedMessage.getAuthor().getName())
                     .replace("{nickName}", name)
                     .replace("{tag}", receivedMessage.getAuthor().getAsTag())
@@ -114,7 +156,11 @@ public class DiscordListener extends ListenerAdapter {
                 extra.addAll(convertedMessage);
                 message.setExtra(extra);
 
-                ProxyServer.getInstance().broadcast(message);
+                if (isAdminChat) {
+                    broadcastBungeeN8Admin(new BaseComponent[]{ message });
+                } else {
+                    ProxyServer.getInstance().broadcast(message);
+                }
             }
 
             receivedMessage.getMessage().getAttachments().forEach((attachment) -> {
@@ -124,33 +170,50 @@ public class DiscordListener extends ListenerAdapter {
                         .append(formattedForMinecraft)
                         .append(url)
                         .create();
-                ProxyServer.getInstance().broadcast(message);
+
+                if (isAdminChat) {
+                    broadcastBungeeN8Admin(message);
+                } else {
+                    ProxyServer.getInstance().broadcast(message);
+                }
             });
 
-            //Discordに再送
-            String nameField = fromDiscordToDiscordName
-                    .replace("{name}", name)
-                    .replace("{nickName}", Objects.requireNonNull(nickname))
-                    .replace("{tag}", receivedMessage.getAuthor().getAsTag())
-                    .replace("{server_name}", receivedMessage.getGuild().getName())
-                    .replace("{channel_name}", receivedMessage.getChannel().getName());
-            String message = receivedMessage.getMessage().getContentRaw();
-            StringJoiner sj = new StringJoiner("\n");
-            receivedMessage.getMessage().getAttachments().forEach(attachment -> sj.add(attachment.getUrl()));
-            String finalMessage = message + "\n" + sj;
-            if (!finalMessage.equals("\n")) {
-                //空白でなければ送信
-                DiscordConnect.getInstance().getDiscordWebhookSenders().forEach(sender ->
-                        sender.sendMessage(
-                                nameField,
-                                receivedMessage.getAuthor().getAvatarUrl(),
-                                finalMessage
-                        )
-                );
+            if (!isAdminChat) {
+                //Discordに再送
+                String nameField = fromDiscordToDiscordName
+                        .replace("{name}", name)
+                        .replace("{nickName}", Objects.requireNonNull(nickname))
+                        .replace("{tag}", receivedMessage.getAuthor().getAsTag())
+                        .replace("{server_name}", receivedMessage.getGuild().getName())
+                        .replace("{channel_name}", receivedMessage.getChannel().getName());
+                String message = receivedMessage.getMessage().getContentRaw();
+                StringJoiner sj = new StringJoiner("\n");
+                receivedMessage.getMessage().getAttachments().forEach(attachment -> sj.add(attachment.getUrl()));
+                String finalMessage = message + "\n" + sj;
+                if (!finalMessage.equals("\n")) {
+                    //空白でなければ送信
+                    DiscordConnect.getInstance().getDiscordWebhookSenders().forEach(sender ->
+                            sender.sendMessage(
+                                    nameField,
+                                    receivedMessage.getAuthor().getAvatarUrl(),
+                                    finalMessage
+                            )
+                    );
+                }
             }
 
             //メッセージを削除
             receivedMessage.getMessage().delete().queue();
         }
+    }
+
+    private void broadcastBungeeN8Admin(BaseComponent[] message) {
+        if (DiscordConnect.getInstance().getChatCasterAPI() == null)
+            return;
+
+        DiscordConnect.getInstance().getProxy().getConsole().sendMessage(message);
+        DiscordConnect.getInstance().getProxy().getPlayers().stream()
+                .filter(p -> p.hasPermission("n8chatcaster.admin"))
+                .forEach(p -> p.sendMessage(message));
     }
 }
